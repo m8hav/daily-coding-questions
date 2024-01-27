d0ce5d08e9ef75a6b0e35478441138a663133edf
Madhav Goyal <103478126+m8hav@users.noreply.github.com>
2024-01-28 01:46:54 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (36.95%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -1,42 +1,22 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         /*
-        optimised solution - O(n)
+        best solution - O(n)
 
-        1. mark all numbers repeated more than 2 times as Integer.MAX_VALUE
-        1.5 count distinct numbers upto 2 times in separate variable k
-        2. move Integer.MAX_VALUE numbers to end with sliding window
-        3. return k
+        Two Pointer Approach:
+        - first pointer to place numbers
+            - starts from beginning
+        - second pointer to traverse and swap with first when number found that isn't equal to number 2 indexes to the left of first pointer
+            - also starts from beginning
+        - return first pointer's position
 
         */
-        int count = 1;
-        int last = nums[0];
-        int k = 1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != last) {
-                count = 1;
-                last = nums[i];
-                k++;
-            }
-            else if (count == 2) nums[i] = Integer.MAX_VALUE;
-            else {
-                count++;
-                k++;
-            }
-        }
-        
-        // moving Integer.MAX_VALUE to end using sliding window
-        int ws = 0;     // window size at start
-        int temp;
-        for (int i = 2; i < nums.length; i++) {
-            if (nums[i] == Integer.MAX_VALUE) ws++;
-            else {
-                temp = nums[i];
-                nums[i] = nums[i - ws];
-                nums[i - ws] = temp;
-            }
-        }
 
-        return k;
+        int i = 2;
+        for (int j = 2; j < nums.length; j++) {
+            if (nums[j] != nums[i - 2])
+                nums[i++] = nums[j];
+        }
+        return i;
     }
 }
