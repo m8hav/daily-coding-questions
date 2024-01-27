2e8bb19c8b9ee7ad7f651be3fb85023253aa5abd
Madhav Goyal <103478126+m8hav@users.noreply.github.com>
2024-01-28 04:53:41 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (61.57%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,5 +1,9 @@
 class Solution {
     public int climbStairs(int n) {
+        /*
+            Tabulation Method - Space Optimised with only 2 variables
+            - Love Babbar's Method
+        */
         int prev2 = 0;
         int prev1 = 1;
         for (int i = 1, ans; i <= n; i++) {
