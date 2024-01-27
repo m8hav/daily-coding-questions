ad914cbba06d213eaba4127aaaf482ab9bf6a10e
Madhav Goyal <103478126+m8hav@users.noreply.github.com>
2024-01-28 01:23:29 +0530

    Time: 2 ms (11.01%) | Memory: 44.6 MB (15.42%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -1,6 +1,14 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        // brute force solution - O(n log(n))
+        /*
+        brute force solution - O(n log(n))
+
+        1. mark all numbers repeated more than 2 times as Integer.MAX_VALUE
+        1.5 count distinct numbers upto 2 times in separate variable k
+        2. sort array
+        3. return k
+
+        */
         int count = 1;
         int last = nums[0];
         int k = 1;
