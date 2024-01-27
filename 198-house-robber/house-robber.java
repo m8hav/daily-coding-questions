5eb5931837a76c9fb31a3b3254d6760eb249570f
Madhav Goyal <103478126+m8hav@users.noreply.github.com>
2024-01-28 05:12:11 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (69.33%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,5 +1,16 @@
 class Solution {
     public int rob(int[] nums) {
+        /*
+            DP Tabulation method - using just last 2 variables
+
+            Two cases - you include a house, or you don't:
+            1. You include a house - you can't add money of last house.
+                - You add money of current house.
+                - You can only add money from the house before the last house.
+            2. You don't include a house - you can add money from last house.
+                - You can't add money of current house.
+        */
+
         int prev2 = 0;
         int prev1 = nums[0];
         for (int i = 1, ans; i < nums.length; i++) {
