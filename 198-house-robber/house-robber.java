c837f251df13c5b15421088e5867289c1096d08a
Madhav Goyal <103478126+m8hav@users.noreply.github.com>
2024-01-28 05:26:57 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (9.46%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,7 +1,55 @@
 class Solution {
-    public int rob(int[] nums) {
+
+    public int solveRec(int[] nums, int idx) {
         /*
-            DP Tabulation method - using just last 2 variables
+            Recursion method - slow AF
+            - Gives TLE
+
+            Two cases - you include a house, or you don't:
+            1. You include a house - you can't add money of last house.
+                - You add money of current house.
+                - You can only add money from the house before the last house.
+            2. You don't include a house - you can add money from last house.
+                - You can't add money of current house.
+        */
+
+        // Base Condition
+        if (idx >= nums.length) return 0;
+
+        // Recursive Condition
+        int incl = nums[idx] + solveRec(nums, idx + 2);
+        int excl = solveRec(nums, idx + 1);
+
+        // Returning best result
+        return Math.max(incl, excl);
+    }
+
+    public int solveMem(int[] nums, int idx, int[] dp) {
+        /*
+            DP Memoization - saving results recursively to DP array
+        */
+        // Base Condition
+        if (idx >= nums.length) return 0;
+
+        // Checking if value already exists in DP array
+        if (dp[idx] != -1) return dp[idx];
+
+        // Recursive Condition
+        int incl = nums[idx] + solveMem(nums, idx + 2, dp);
+        int excl = solveMem(nums, idx + 1, dp);
+
+        // Saving best result to DP array
+        dp[idx] = Math.max(incl, excl);
+
+        // Returning best result
+        return dp[idx];
+    }
+
+    public int solveTabSpaceOp(int[] nums) {
+        /*
+            DP Tabulation method - Space Optimized
+            - using just last 2 variables
+            - FASTEST
 
             Two cases - you include a house, or you don't:
             1. You include a house - you can't add money of last house.
@@ -19,5 +67,15 @@ class Solution {
             prev1 = ans;
         }
         return prev1;
+    }
+
+    public int rob(int[] nums) {
+        // Creating DP array
+        int[] dp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++)
+            dp[i] = -1;
+
+        // return solveRec(nums, 0);    // Gives TLE
+        return solveMem(nums, 0, dp);
     }
 }
