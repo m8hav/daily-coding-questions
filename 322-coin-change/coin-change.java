e0ecbd26fe11a3492ddccebacf15be424f93c3cc
Madhav Goyal <103478126+m8hav@users.noreply.github.com>
2024-01-28 06:40:57 +0530

    Time: 16 ms (66.81%) | Memory: 44.4 MB (45.60%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -5,8 +5,8 @@ class Solution {
             dp[value] = Integer.MAX_VALUE;
         dp[0] = 0;
 
-        for (int coin : coins) {
-            for (int value = 1; value <= amount; value++) {
+        for (int value = 1; value <= amount; value++) {
+            for (int coin : coins) {
                 if (value - coin < 0 || dp[value - coin] == Integer.MAX_VALUE)
                     continue;
                 dp[value] = Math.min(dp[value], 1 + dp[value - coin]);
